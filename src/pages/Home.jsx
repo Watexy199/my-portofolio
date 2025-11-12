ef83442fd3aa4e4a251f135eafb6f1f811ae0ea0
Talent Mandishe <talentymandishe@gmail.com>
2025-11-13 01:31:05 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { StarBackground } from "../components/StarBackground"
+import { ThemeToggle } from "@/components/ThemeToggle"
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/**Theme Toogle */}
+        <ThemeToggle/>
+
+        {/**Background Effects */}
+        <StarBackground/>
+
+        {/**Navbar */}
+
+        {/**Main Content */}
+
+        {/**Footer */}
+    </div>
+}
